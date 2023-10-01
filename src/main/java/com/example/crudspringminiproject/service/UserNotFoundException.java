73bd11e698a5026c5cab0dc2a4f1501a7e5929f5
Tran Anh Quang <dinosaurous9x@gmail.com>
2023-10-02 01:13:34 +0700

    mini-crud

---
+++ src/main/java/com/example/crudspringminiproject/service/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package com.example.crudspringminiproject.service;
+
+public class UserNotFoundException extends Throwable {
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ src/test/java/com/example/crudspringminiproject/UserRepositoryTest.java
@@ -0,0 +1,77 @@
+package com.example.crudspringminiproject;
+
+import com.example.crudspringminiproject.entity.User;
+import com.example.crudspringminiproject.repository.UserRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Rollback;
+
+import java.util.Optional;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Rollback(value = false)
+public class UserRepositoryTest {
+
+    @Autowired
+    private UserRepository repo;
+
+    @Test
+    public void testAddNew(){
+        User user = new User();
+        user.setEmail("dinosaurous9x@gmail.com");
+        user.setPassword("123456");
+        user.setFirstName("Quang");
+        user.setLastName("Tran");
+
+        user.setEmail("bigdaddy@gmail.com");
+        user.setPassword("123456");
+        user.setFirstName("M3p");
+        user.setLastName("Luis");
+
+        User savedUser = repo.save(user);
+        Assertions.assertThat(savedUser).isNotNull();
+        Assertions.assertThat(savedUser.getId()).isGreaterThan(0);
+    }
+
+    @Test
+    public void testListAll(){
+        Iterable<User> users = repo.findAll();
+        Assertions.assertThat(users).hasSizeGreaterThan(0);
+
+        for (User user : users){
+            System.out.println(user);
+        }
+    }
+
+    @Test
+    public void testUpdate(){
+        Integer userId = 1;
+        Optional<User> optionalUser = repo.findById(userId);
+        User user = optionalUser.get();
+        user.setPassword("qwer123");
+        repo.save(user);
+
+        User updatedUser = repo.findById(userId).get();
+        Assertions.assertThat(updatedUser.getPassword()).isEqualTo("qwer123");
+    }
+
+    @Test
+    public void testGet(){
+        Integer userId = 1;
+        Optional<User> optionalUser = repo.findById(userId);
+        Assertions.assertThat(optionalUser).isPresent();
+        System.out.println(optionalUser.get());
+    }
+
+    @Test
+    public void testDelete(){
+        Integer userId = 2;
+        repo.deleteById(userId);
+        Optional<User> optionalUser = repo.findById(userId);
+        Assertions.assertThat(optionalUser).isNotPresent();
+    }
+}
